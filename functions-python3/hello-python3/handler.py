15cf44c09dec933bfc4c35c7c8818dcac1ebbe73
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-06-28 19:07:31 -0400

    Update to use requests

--- functions-python3/hello-python3/handler.py
+++ functions-python3/hello-python3/handler.py
@@ -1,7 +1,12 @@
+import requests
+import json
+
 def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
-    print("Hello! You said:", req)
+    result = {"found": False}
+    json_req = json.loads(req)
+    r = requests.get(json_req["url"])
+    if json_req["term"] in r.text:
+        result = {"found": True}
+
+    print(json.dumps(result))
     return req
