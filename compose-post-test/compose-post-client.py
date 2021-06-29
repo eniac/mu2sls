e4481cb6aed82f66ae45af7ff67c0dd44f4da3ff
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-06-29 07:53:41 -0400

    Update the client to print its result

--- compose-post-test/compose-post-client.py
+++ compose-post-test/compose-post-client.py
@@ -11,9 +11,13 @@ from social_network import TextService
 URL="http://127.0.0.1:8080/function/compose-post"
 
 
-
+## An HTTP Client is necessary to interact with FaaS
 transport = THttpClient.THttpClient(URL)
+
+## TODO: Transport probably doesn't really matter
 transport = TTransport.TBufferedTransport(transport)
+
+## The protocol also probably doesn't matter, but it is good to start with JSON for better observability.
 protocol = TJSONProtocol.TJSONProtocol(transport)
 client = TextService.Client(protocol)
 
@@ -21,4 +25,6 @@ client = TextService.Client(protocol)
 transport.open()
 
 ## Make a request
-client.ComposeText(5, "popopo", {})
+ret = client.ComposeText(5, "popopo", {})
+
+print(ret)
