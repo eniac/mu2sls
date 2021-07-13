0156b9aeb71072f73f0ca839c9474806c3a4653c
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-07-13 17:13:11 -0400

    Find the fields of a service

--- python-interposition-experiment/type-annotations-experiment.py
+++ python-interposition-experiment/type-annotations-experiment.py
@@ -1,4 +1,8 @@
 import ast
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+# logging.basicConfig(level=logging.DEBUG)
 
 test_source_file = "type-annotations-experiment-test.py"
 
@@ -10,6 +14,8 @@ test_ast = ast.parse(test_source,
                      type_comments=True)
 
 # print(ast.dump(test_ast))
+
+## TODO: Maybe move all the static methods to an ast_util file
 
 class ServiceClassFinder(ast.NodeVisitor):
     def __init__(self):
@@ -59,21 +65,96 @@ def find_services(ast_node):
     return service_finder.services
 
 
+## This is a class that describes the state of an object.
+##
+## TODO: Figure out what the correct structure is for this class
+##
+## TODO: This class also probably needs to contain the initialization functions for all the fields.
+##       These initialization ASTs need to be deterministic and pure.
+class ServiceState:
+    def __init__(self):
+        self.persistent_fields = {}
+        self.temporary_fields = {}
+        self.thrift_clients = {}
+    
+    def __str__(self):
+        ret_list = ["Service State:"]
+        ret_list.append("|-- Persistent: " + str(self.persistent_fields))
+        ret_list.append("|-- Temporary: " + str(self.temporary_fields))
+        ret_list.append("|-- Thrift Clients: " + str(self.thrift_clients))
+        return "\n".join(ret_list)
+
+    ## TODO: Do we need an initialization function for persistent field??
+    def add_persistent_field(self, name, init_ast):
+        self.persistent_fields[name] = init_ast
+
+    ## TODO: What other fields do we need here? Maybe the actual client name?
+    def add_thrift_client(self, name, init_ast):
+        self.thrift_clients[name] = init_ast
+
+    ## This function determines what kind of field that is (based on the type annotation)    
+    def add_field(self, name, init_ast, type):
+        ## TODO: Make that more robust
+        if(type.startswith('Persistent')):
+            self.add_persistent_field(name, init_ast)
+        elif(type == 'ThriftClient'):
+            self.add_thrift_client(name, init_ast)
+        else:
+            assert(False)
+
 ## A class that finds all the fields of a service object and separates them into persistent and not persistent.
 ##
 ## Assumption: All fields are initialized when calling __init__
-class FieldFinder(ast.NodeVisitor):
+class StateFinder(ast.NodeVisitor):
+    def __init__(self):
+        ## Contains the state of a service, namely its persistent and non-persistent fields,
+        ## as well as its thrift clients (which are also non-persistent fields).
+        self.service_state = ServiceState()
+        self.in_init = False
+
+    @staticmethod
+    def is_self_name(node):
+        ## TODO: What about ctx?
+        # Name(id='self', ctx=Load())
+        cond = (isinstance(node, ast.Name)
+                and node.id == 'self')
+        return cond
+
     ## This function finds the __init__ and then searches for all field initializations in it.
     def visit_FunctionDef(self, node: ast.FunctionDef):
         if(node.name == '__init__'):
-            print("Function init:", node.name)
-            print(ast.dump(node))
+            ## We don't expect function definitions in __init__
+            assert(not self.in_init)
+            logging.debug("Function init: " + node.name)
+            # print(ast.dump(node))
 
-        ## We don't actually want to visit the children so we don't need the following
-        # self.generic_visit(node)
+            self.in_init = True
+            ## TODO: Assert that __init__ only takes self as an argument
 
+            ## TODO: Populate service fields
+            ## We only need to visit the children in the __init__ function
+            self.generic_visit(node)
 
+    ## Is there any other statement node that we want to follow?
+    def visit_Assign(self, node: ast.Assign):
+        ## We don't expect any assignments outside of __init__
+        assert(self.in_init)
 
+        # print(ast.dump(node))
+
+        ## TODO: Relax the following assumptions accordingly
+
+        ## Only a single target (lval)
+        assert(len(node.targets) == 1)
+        target = node.targets[0]
+        ## The lval needs to be an attribute access to self
+        assert(isinstance(target, ast.Attribute))
+        assert(StateFinder.is_self_name(target.value))
+        field_name = target.attr
+        logging.debug("Field name: " + field_name)
+        init_ast = target.value
+        logging.debug("Init AST: " + ast.dump(init_ast))
+        self.service_state.add_field(field_name, init_ast, node.type_comment)
 
 
 all_services = find_services(test_ast)
@@ -82,8 +163,10 @@ for service in all_services:
     # print(ast.dump(service))
 
     ## TODO: Find init
-    field_finder = FieldFinder()
+    field_finder = StateFinder()
     field_finder.visit(service)
+    service_state = field_finder.service_state
+    print("Service State:", service_state)
 
 
 ## High level Q: Is it possible to perform all changes dynamically? That is, instead of
