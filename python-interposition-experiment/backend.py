ea7729390e4063fdf4624f73a74af5fb57801d53
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-07-15 18:20:12 -0400

    Create a backend that simply decompiles a Python AST to source code

---
+++ python-interposition-experiment/backend.py
@@ -0,0 +1,22 @@
+import sys
+
+from uncompyle6.main import decompile
+
+## This function takes an AST and saves python_source code in the out_file
+def ast_to_source(ast_node, out_file):
+    ## TODO: Do we need the filename
+    code_object = compile(ast_node, filename="<ast>", mode="exec")
+
+    ## TODO: Do we actually need the bytecode version?
+    ##       By default uncompyle should use the interpreter version.
+    ##
+    ## TODO: It seems that uncompyle does not produce comments in the code,
+    ##       meaning that they are lost.
+    bytecode_version=None
+    with open(out_file, "w") as out_f:
+        decompiled_code = decompile(bytecode_version,
+                                    code_object,
+                                    out=out_f)
+    
+    ## TODO: I am not sure what this object is and whether we need it
+    return decompiled_code
--- python-interposition-experiment/type-annotations-experiment.py
+++ python-interposition-experiment/type-annotations-experiment.py
@@ -1,14 +1,27 @@
 import ast
 import logging
+import os
 
 
 import frontend
+import backend
 
+## This might be needed for the decompiler
+PYTHON_VERSION="3.8"
 logging.basicConfig(level=logging.WARNING)
 # logging.basicConfig(level=logging.DEBUG)
 
+## TODO: These should be given as inputs
 test_source_file = "type-annotations-experiment-test.py"
+out_dir = "target"
+out_file = os.path.join(out_dir, "test.py")
 
+
+## Create the out directory if it doesn't exist
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+## Read the input file and parse it
 with open(test_source_file) as f:
     test_source = f.read()
 
@@ -24,6 +37,10 @@ service_states = frontend.parse_services(test_ast)
 for service_state in service_states:
     print("Service State:", service_state)
 
+
+## I don't think we need this object.
+_decompiled = backend.ast_to_source(test_ast, out_file)
+
 ## TODO: Start with a rudimentary backend that simply prints back the code, making sure that initializations happens in the beginning.
 ##
 ## Q: Do we need anything else in the header?
@@ -32,6 +49,8 @@ for service_state in service_states:
 ##    or at compile time.
 
 ## TODO: Make a trivial extension where accesses to the persistent fields go through getters and setters. Test it.
+
+## TODO: Package these things in a normal library
 
 ## High level Q: Is it possible to perform all changes dynamically? That is, instead of
 ##               compiling the AST, simply decorating the class and making sure that it implements
