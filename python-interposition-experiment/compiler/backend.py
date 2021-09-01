99481b826e1af6171a1bb464e5ec25adb8156aa7
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-09-01 20:04:17 +0300

    Add an init function in the compiled service

--- python-interposition-experiment/compiler/backend.py
+++ python-interposition-experiment/compiler/backend.py
@@ -43,11 +43,14 @@ def field_store_key_name(field_name: str) -> str:
 def construct_descriptor_ast(field_name: str):
     descriptor_module_ast = ast.parse("class " + descriptor_class_name(field_name) + ":" """
         def __get__(self, obj, objtype=None):
+            logging.info('Accessing collection')
             value = obj._wrapper_""" + field_name + """
             return value
 
         def __set__(self, obj, value):
+            logging.info('Setting collection')
             if(isinstance(value, wrappers.WrapperTerminal)):
+                logging.info('Collection initialized')
                 obj._wrapper_""" + field_name + """ = value
             else:
                 obj._wrapper_""" + field_name + """._wrapper_set(value)""")
@@ -61,11 +64,54 @@ def descriptor_init_value_ast(field_name: str):
     return init_val_ast
 
 def persistent_object_target_init_ast(persistent_object_name, _persistent_object_init_ast):
-    ## TODO: Replace that with the proper descriptor initialization value
     value = descriptor_init_value_ast(persistent_object_name)
     assign_node = ast.Assign(targets=[ast.Name(id=persistent_object_name, ctx=ast.Store())],
                              value=value)
     return assign_node
+
+def construct_init_method_persistent_object_ast(per_obj_name: str, per_obj_init_ast: ast.AST):
+    beldi_key_var_name = per_obj_name + '_key'
+    init_val_var_name = per_obj_name + "_init_val"
+
+    # collection_key = "test-collection"
+    assgn1 = ast.Assign(targets=[ast.Name(id=beldi_key_var_name, ctx=ast.Store())], 
+                        value=ast.Constant(value=field_store_key_name(per_obj_name), kind=None), type_comment=None)
+    
+    # collection_init_val = []
+    assgn2 = ast.Assign(targets=[ast.Name(id=init_val_var_name, ctx=ast.Store())], 
+                        value=per_obj_init_ast, type_comment=None)
+    
+    # self.collection = wrappers.wrap_terminal(collection_key, collection_init_val, self.beldi)
+    assgn3 = ast.Assign(targets=[ast.Attribute(value=ast.Name(id='self', ctx=ast.Load()), attr=per_obj_name, ctx=ast.Store())], 
+                        value=ast.Call(func=ast.Attribute(value=ast.Name(id='wrappers', ctx=ast.Load()), attr='wrap_terminal', ctx=ast.Load()), 
+                                       args=[ast.Name(id=beldi_key_var_name, ctx=ast.Load()), 
+                                             ast.Name(id=init_val_var_name, ctx=ast.Load()),
+                                             ast.Attribute(value=ast.Name(id='self', ctx=ast.Load()), attr='beldi', ctx=ast.Load())],
+                                       keywords=[]), 
+                        type_comment=None)
+
+    return [assgn1, assgn2, assgn3]
+
+def construct_init_method_ast(persistent_objects):
+    body = []
+
+    ## First initialize Beldi
+    beldi_ass_module_ast = ast.parse("beldi = beldi_stub.Beldi()")
+    beldi_ass_ast = extract_single_stmt_from_module(beldi_ass_module_ast)
+    body.append(beldi_ass_ast)
+
+    ## Then initialize all the objects
+    for per_obj_name, per_obj_init_ast in persistent_objects.items():
+        body += construct_init_method_persistent_object_ast(per_obj_name, per_obj_init_ast)
+
+    ## Create the function
+    function_ast = ast.FunctionDef(name='__init__', 
+                                   args=ast.arguments(posonlyargs=[], 
+                                                      args=[ast.arg(arg='self', annotation=None, type_comment=None)], 
+                                                      vararg=None, kwonlyargs=[], kw_defaults=[], kwarg=None, defaults=[]), 
+                                   body=body, 
+                                   decorator_list=[], returns=None, type_comment=None)
+    return function_ast
 
 ## TODO: Not sure if this should be a method of Service or a function here
 def service_to_ast(service: Service):
@@ -85,9 +131,12 @@ def service_to_ast(service: Service):
 
     ## TODO: Do something about thrift
 
-    ## TODO: Create an init function that initializes beldi and all the objects
-    
-    body = assignments + service.methods
+    ## Creates an init function that initializes beldi and all the objects
+    ##
+    ## TODO: This should take some form of configuration to use beldi or not
+    init_method = construct_init_method_ast(persistent_objects)
+
+    body = assignments + [init_method] + service.methods
     
     new_class = ast.ClassDef(name=service.name(),
                              bases=service.bases(),
