fb1494f339f95f21c0e9387b13add77dc8046250
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-09-05 05:42:42 -0400

    Add args that don't do anything now

--- mu2sls/mu2sls.py
+++ mu2sls/mu2sls.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
+import argparse
 import logging
 import os
 
 from compiler import compiler
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="the input file to be compiled (must contain a service)")
+    parser.add_argument("output", help="the output file that will contain a serverless handler")
+    parser.add_argument("-l", "--log_level",
+                        help="the logging level",
+                        choices=['debug', 'info', 'warn', 'error'],
+                        default='info')
+    parser.add_argument("-s", "--sls_backend",
+                        help="the serverless backend",
+                        choices=['openfaas'],
+                        default='openfaas')
+    args = parser.parse_args()
+
+    ## Print all the arguments before they are modified below
+    logging.debug("Arguments:")
+    for arg_name, arg_val in vars(args).items():
+        logging.debug(arg_name + str(arg_val))
+    logging.debug("-" * 40)
+    return args
+
+
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-    test_source_file = "tests/source_specs/list-service.py"
-    out_dir = "target"
-    out_file = os.path.join(out_dir, "popo.py")
+    args = parse_args()
+    source_file = args.input
+    out_file = args.output
 
+    ## TODO: Do something with the -s and -l args
 
-    ## Create the out directory if it doesn't exist
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-
-    compiler.compile_service_module(test_source_file, out_file)
+    compiler.compile_service_module(source_file, out_file)
 
 if __name__ == "__main__":
     main()
