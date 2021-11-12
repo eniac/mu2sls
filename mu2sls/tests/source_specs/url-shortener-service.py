456a7f27cf72c93607a89202e72a79587b0375d2
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-11-12 13:15:32 -0500

    Add the source spec of the url-shortener-service:

---
+++ mu2sls/tests/source_specs/url-shortener-service.py
@@ -0,0 +1,19 @@
+from compiler import decorators
+
+@decorators.service
+class Service:
+    def __init__(self):
+        self.urls = {} # type: Persistent[dict]
+
+    ## This method returns a short url, 
+    ##   either by making it on the spot, or by looking in the dict.
+    ##
+    ## TODO: Enable transactoions in the source spec of the compiler
+    def method(self, long_url):
+        
+        ## TODO: Add transactions
+        if not long_url in self.urls:
+            ## TODO: Do that in a separate method
+            short_url = long_url + "+hash"
+            self.urls[long_url] = short_url
+        return self.urls[long_url]
