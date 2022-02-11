6e9d43170daa478ecf677994218b0bf5f3a96f2f
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-02-11 07:30:58 -0500

    Add the test in the main testing suite

--- mu2sls/run_tests.sh
+++ mu2sls/run_tests.sh
@@ -10,4 +10,7 @@ mkdir -p "${test_log_dir}"
 test_log="${test_log_dir}/test_media_service.log"
 
 echo "Application integration tests... logs in \"${test_log}\""
-./tests/test_media_service.sh >"${test_log}" 2>&1 || echo "Error: Media service test failed"
+echo "Media service with local store..."
+./tests/test_media_service.sh >"${test_log}" 2>&1 || { echo "Error: Media service test failed"; exit 1; }
+echo "Media service with local Beldi FDB store..."
+./tests/test_media_service.sh beldi >>"${test_log}" 2>&1 || { echo "Error: Media service test (with beldi) failed"; exit 1; }
