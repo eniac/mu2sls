400be6cf281a0f31755da068b061002b834deddb
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-02-14 09:10:55 -0500

    Fix a bug where copyfile failed for directories

--- mu2sls/scripts/knative_dev.py
+++ mu2sls/scripts/knative_dev.py
@@ -8,25 +8,30 @@ def run(cmd):
 
 HOME = run(["git", "rev-parse", "--show-toplevel", "--show-superproject-working-tree"])
 MUSLS = os.path.join(HOME, "mu2sls")
+TARGET_DIR = os.path.join(MUSLS, "target")
 
+
+## TODO: Pass the csv as an argument and move that to a shell script maybe?
 def compile():
     compiler = os.path.join(MUSLS, "scripts/compile_services.sh")
     deploy = os.path.join(MUSLS, "tests/media-service-test.csv")
-    target = os.path.join(MUSLS, "target")
     sls_backend = "knative"
-    res = run(["bash", compiler, deploy, target, "-s", sls_backend])
+    res = run(["bash", compiler, deploy, TARGET_DIR, "-s", sls_backend])
     print(res)
 
 def prepare():
     kmedia = os.path.join(MUSLS, "kmedia")
     shutil.rmtree(kmedia, ignore_errors=True)
     os.mkdir(kmedia)
-    filenames = os.listdir(os.path.join(MUSLS, "target"))
+    filenames = [fn for fn in os.listdir(TARGET_DIR)
+                 if os.path.isfile(os.path.join(TARGET_DIR, fn))]
     for fn in filenames:
         name = fn.split(".")[0]
         os.mkdir(os.path.join(kmedia, name))
         with open(os.path.join(MUSLS, "Dockerfile"), "r") as f:
             dockerfile = f.read()
+        ## TODO: Do that properly by adding a reference to an 
+        ##       environment variable in the Dockerfile
         dockerfile = dockerfile.replace("PLACEHODLER", name)
         with open(os.path.join(kmedia, name, "Dockerfile"), "w") as f:
             f.write(dockerfile)
