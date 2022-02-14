7ce3f7d4c8f4f91df622c4fa6063c4e452fc64a3
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-02-14 09:15:16 -0500

    Add temporary compiled files in a temporary directory instead of in a directory in mu2sls

--- mu2sls/scripts/knative_dev.py
+++ mu2sls/scripts/knative_dev.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+import tempfile
 
 def run(cmd):
     res = subprocess.run(cmd, stdout=subprocess.PIPE)
@@ -8,23 +9,21 @@ def run(cmd):
 
 HOME = run(["git", "rev-parse", "--show-toplevel", "--show-superproject-working-tree"])
 MUSLS = os.path.join(HOME, "mu2sls")
-TARGET_DIR = os.path.join(MUSLS, "target")
-
 
 ## TODO: Pass the csv as an argument and move that to a shell script maybe?
-def compile():
+def compile(target_dir):
     compiler = os.path.join(MUSLS, "scripts/compile_services.sh")
     deploy = os.path.join(MUSLS, "tests/media-service-test.csv")
     sls_backend = "knative"
-    res = run(["bash", compiler, deploy, TARGET_DIR, "-s", sls_backend])
+    res = run(["bash", compiler, deploy, target_dir, "-s", sls_backend])
     print(res)
 
-def prepare():
+def prepare(target_dir):
     kmedia = os.path.join(MUSLS, "kmedia")
     shutil.rmtree(kmedia, ignore_errors=True)
     os.mkdir(kmedia)
-    filenames = [fn for fn in os.listdir(TARGET_DIR)
-                 if os.path.isfile(os.path.join(TARGET_DIR, fn))]
+    filenames = [fn for fn in os.listdir(target_dir)
+                 if os.path.isfile(os.path.join(target_dir, fn))]
     for fn in filenames:
         name = fn.split(".")[0]
         os.mkdir(os.path.join(kmedia, name))
@@ -39,8 +38,10 @@ def prepare():
 
 
 def main():
-    compile()
-    prepare()
+    target_dir = tempfile.mkdtemp()
+    compile(target_dir)
+    prepare(target_dir)
+    shutil.rmtree(target_dir)
 
 if __name__ == '__main__':
     main()
