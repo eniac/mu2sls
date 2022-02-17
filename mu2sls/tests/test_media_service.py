0e647d5d4505de96109d529ef83ed4d0e597c85c
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-02-17 16:50:30 -0500

    Modify test to be able to also test knative deployment

--- mu2sls/tests/test_media_service.py
+++ mu2sls/tests/test_media_service.py
@@ -1,7 +1,9 @@
 import json
 import os
+import sys
 
 from scripts.local_dev import *
+from scripts import knative_dev
 import runtime.knative.invoke as knative_invoke_lib
 import runtime.local.invoke as local_invoke_lib
 
@@ -10,13 +12,45 @@ DEPLOYMENT_FILE = f'{MU2SLS_TOP}/tests/media-service-test.csv'
 
 ## TODO: Extend it to do the calls using SyncInvoke maybe?
 ##       Then it would be possible to use it to test non-local deployments too.
-def main(store_conf="local"):
-    deployed_services = deploy_from_deployment_file(DEPLOYMENT_FILE, store_conf)
+def main(store_conf="local", docker_io_username='default'):
 
-    # print(deployed_services)
+    ## For local or local-beldi testing, we need to deploy
+    if(store_conf in ["local", "beldi"]):
+        deployed_services = deploy_from_deployment_file(DEPLOYMENT_FILE, store_conf)
+        # print(deployed_services)
 
-    run_test_deployed_services(deployed_services, local_invoke_lib)
-    run_local_deployment_tests(deployed_services)
+        run_test_deployed_services(deployed_services, local_invoke_lib)
+        run_local_deployment_tests(deployed_services)
+    elif(store_conf == "knative"):
+        deployment_list = [('castinfo', 'cast_info'),
+                           ('composereview', 'compose_review'),
+                           ('frontend', 'frontend'),
+                           ('movieid', 'movie_id'),
+                           ('movieinfo', 'movie_info'),
+                           ('moviereview', 'movie_review'),
+                           ('page', 'page'),
+                           ('plot', 'plot'),
+                           ('rating', 'rating'),
+                           ('reviewstorage', 'review_storage'),
+                           ('text', 'text'),
+                           ('uniqueid', 'unique_id'),
+                           ('user', 'user'),
+                           ('userreview', 'user_review')]
+        knative_dev.deploy_services(docker_io_username, deployment_list)
+
+
+        service_list = ["CastInfo", "ComposeReview", "Frontend", 
+                        "MovieId", "MovieInfo", "MovieReview", 
+                        "Page", "Plot", "Rating", 
+                        "ReviewStorage", "Text", "UniqueId", 
+                        "User", "UserReview"]
+        services = {k: k for k in service_list}
+        run_test_deployed_services(services, knative_invoke_lib)
+    else:
+        print("Unrecognized test conf")
+        exit(1)
+
+
     
 def run_test_deployed_services(deployed_services, invoke_lib):
     username = "red_ad"
