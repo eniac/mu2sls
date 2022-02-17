0abf1eb4937b5c94404388ca4f4918c82875c251
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-02-17 15:38:04 -0500

    Refactor the media_servive test to also work with remote invocations

--- mu2sls/tests/test_media_service.py
+++ mu2sls/tests/test_media_service.py
@@ -2,6 +2,8 @@ import json
 import os
 
 from scripts.local_dev import *
+import runtime.knative.invoke as knative_invoke_lib
+import runtime.local.invoke as local_invoke_lib
 
 MU2SLS_TOP = os.getenv('MU2SLS_TOP')
 DEPLOYMENT_FILE = f'{MU2SLS_TOP}/tests/media-service-test.csv'
@@ -13,30 +15,57 @@ def main(store_conf="local"):
 
     # print(deployed_services)
 
+    run_test_deployed_services(deployed_services, local_invoke_lib)
+    run_local_deployment_tests(deployed_services)
+    
+def run_test_deployed_services(deployed_services, invoke_lib):
     username = "red_ad"
     password = "1234"
 
     ## Login with user
-    deployed_services['User'].register_user("Red", "Adams", username, password)
-    assert list(deployed_services['User'].users.values())[0]['first_name'] == "Red"
+    invoke_lib.SyncInvoke(deployed_services['User'], "register_user", "Red", "Adams", username, password)
 
     ## Add movie
-    deployed_services['MovieId'].register_movie_id("Titanic", '42')
-    assert 'Titanic' in deployed_services['MovieId'].movie_ids.keys()
+    invoke_lib.SyncInvoke(deployed_services['MovieId'], 
+                          "register_movie_id", 
+                          "Titanic", '42')
 
     ## Add plot and movie info
-    info = json.loads('{"movie_id": "42", "title": "Titanic", "casts": [], "plot_id": "299534", "thumbnail_ids": ["/or06FN3Dka5tukK1e9sl16pB3iy.jpg"], "photo_ids": [], "video_ids": [], "avg_rating": 8.6, "num_rating": 4789}')
-    deployed_services['MovieInfo'].write_movie_info(info)
-    assert sorted(list(deployed_services['MovieInfo'].movie_infos.get("42").items())) == sorted(list(info.items()))
-    deployed_services['Plot'].write_plot(info['plot_id'], "ship hits iceberg")
-    assert deployed_services['Plot'].plots.get("299534") == 'ship hits iceberg'
+    info = json.loads('{"movie_id": "42", "title": "Titanic", "casts": [], "plot_id": "299534", "thumbnail_ids": ["/or06FN3Dka5tukK1e9sl16pB3iy.jpg"], "photo_ids": [], "video_ids": [], "avg_rating": 8.6, "num_rating": 4789}')    
+    invoke_lib.SyncInvoke(deployed_services['MovieInfo'], 
+                          "write_movie_info", 
+                          info)
+
+    invoke_lib.SyncInvoke(deployed_services['Plot'],
+                          "write_plot",
+                          info['plot_id'], "ship hits iceberg")
+
+    ret = invoke_lib.SyncInvoke(deployed_services['Plot'],
+                                "read_plot",
+                                info['plot_id'])
+    assert ret == 'ship hits iceberg'
 
     ## Compose Review
-    deployed_services['Frontend'].compose(username, password, "Titanic", 5, "Titanic is the worst movie I have ever watched!")
-    assert len(deployed_services['ComposeReview'].reqs.items()) == 0
+    invoke_lib.SyncInvoke(deployed_services['Frontend'],
+                          "compose",
+                          username, password, "Titanic", 5, "Titanic is the worst movie I have ever watched!")
 
     ## TODO: Can use populate.py and compressed.json to populate movies and users
-    
+
+## These tests can only be ran if we have the instances too
+def run_local_deployment_tests(deployed_services):
+    assert list(deployed_services['User'].users.values())[0]['first_name'] == "Red"
+
+    assert 'Titanic' in deployed_services['MovieId'].movie_ids.keys()
+
+    info = json.loads('{"movie_id": "42", "title": "Titanic", "casts": [], "plot_id": "299534", "thumbnail_ids": ["/or06FN3Dka5tukK1e9sl16pB3iy.jpg"], "photo_ids": [], "video_ids": [], "avg_rating": 8.6, "num_rating": 4789}')    
+
+    assert sorted(list(deployed_services['MovieInfo'].movie_infos.get("42").items())) == sorted(list(info.items()))
+
+    assert deployed_services['Plot'].plots.get("299534") == 'ship hits iceberg'
+
+    assert len(deployed_services['ComposeReview'].reqs.items()) == 0
+
 
 if __name__ == '__main__':
     assert(len(sys.argv) in range(1,3))
