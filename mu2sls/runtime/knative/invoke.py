68c7302f2ebe8fddf468d851afa67fdea16d0347
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-02-17 15:20:10 -0500

    invocations working on knative

--- mu2sls/runtime/knative/invoke.py
+++ mu2sls/runtime/knative/invoke.py
@@ -5,16 +5,20 @@ def SyncInvoke(client, method_name: str, *args):
     ip = os.environ.get('LOAD_BALANCER_IP')
     assert ip is not None
     client = client.lower()
-    res = requests.get(f'http://{ip}/{method_name}', headers={"Host": f"{client}.default.example.com"},
-                       params={"args": args}).json()
-    # res = requests.get(f'http://127.0.0.1:5000/{method_name}', params={"args": args}).json()
+    ## Note: This is obsolete. We are now passing data using json.
+    # res = requests.get(f'http://{ip}/{method_name}', headers={"Host": f"{client}.default.example.com"},
+    #                    params={"args": args}).json()
+    res = requests.post(f'http://{ip}/{method_name}', 
+                        headers={"Host": f"{client}.default.example.com"},
+                        json={"args": args}).json()
     return res
 
 def AsyncInvoke(client, method_name: str, *args):
-    return SyncInvoke(client, method_name, *args)
+    return (lambda: SyncInvoke(client, method_name, *args))
 
 def Wait(promise):
-    return
+    return promise()
 
 def WaitAll(*promises):
-    return
+    rets = [p() for p in promises]
+    return rets
