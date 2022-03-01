5c554a3167062a17fcdf0f1909b8eb2c071be14a
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-03-01 17:42:18 -0500

    Add some checking that deserialized values are not empty

--- mu2sls/runtime/serde.py
+++ mu2sls/runtime/serde.py
@@ -3,9 +3,13 @@ import json
 def serialize(item):
     return json.dumps(item).encode()
 
-
 def deserialize(bitem: bytes):
-    return json.loads(bitem.decode())
+    decoded_string = bitem.decode()
+    ## json.loads() cannot handle an empty string,
+    ##   so we make sure that we only pass a non-empty string to it
+    if decoded_string == '':
+        return None
+    return json.loads(decoded_string)
 
 
 ## Old way of doing serialization/deserialization
