6fccd07a1704d7e11c0fc4c9480d60c3dfad1580
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-03-05 03:42:25 -0500

    Add a potential alternative for doing the first commit

--- mu2sls/runtime/wrappers.py
+++ mu2sls/runtime/wrappers.py
@@ -52,6 +52,8 @@ class WrapperTerminal(object):
         ## TODO: Since Python is not lazy, the init_val is always evaluated and we might want to avoid that if it is a performance bottleneck.
 
         ## Initialize the collection if it doesn't already exist in Beldi
+        ## Potentially alternative way of doing it (though, we don't have a request id maybe here.)
+        # val = store.read_until_success(self._wrapper_obj_key)
         if(not store.contains(self._wrapper_obj_key)):
             print("Store doesn't contain key:", self._wrapper_obj_key)
             ## NOTE: We need to use set_if_not_exists to ensure atomicity
