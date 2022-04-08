645f0faa77cda77827c76158873e3395816440b8
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-04-08 19:18:40 -0400

    Add a BUG TODO in serde

--- mu2sls/runtime/serde.py
+++ mu2sls/runtime/serde.py
@@ -3,10 +3,15 @@ import json
 from runtime.beldi.common import *
 
 def serialize(item):
+    # print("Serialize:", item, "to:", json.dumps(item))
+    ## TODO: There is a bug with this serialization :')
+    ## It serializes keys to dictionaries as strings...
+    ## TODO: We should use pickle, I don't know why we switched
     return json.dumps(item).encode()
 
 def deserialize(bitem: bytes):
     decoded_string = bitem.decode()
+    # print("Deserialized:", decoded_string, "to:", json.loads(decoded_string))
     return json.loads(decoded_string)
 
 
