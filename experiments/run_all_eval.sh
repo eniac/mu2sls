3b3ab30f8880fb71c83c618e8e8245958aef5179
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-09-29 02:56:42 -0400

    make sure to save the outputs of media and hotel expeirments

--- experiments/run_all_eval.sh
+++ experiments/run_all_eval.sh
@@ -3,7 +3,7 @@ bash eval_small_applications.sh
 sleep 60
 
 ## Run the real-world applications
-bash eval_media.sh
+bash eval_media.sh | tee media-service-test.log
 sleep 60
-bash eval_hotel_reservation.sh
+bash eval_hotel_reservation.sh | hotel-reservation.log
 sleep 60
