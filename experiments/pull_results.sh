c32c4fd1b2fdf55b992f587dd482598bd7e2549d
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-09-29 02:55:18 -0400

    Add a script that pulls results

---
+++ experiments/pull_results.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+private_key=${1?Private key path is not given}
+node_username=${2?Node username is not given}
+node_address=${3?Node address is not given}
+
+scp -i ${private_key} ${node_username}@${node_address}:knative/single_stateful.log ./results/single_stateful.log
+scp -i ${private_key} ${node_username}@${node_address}:knative/chain.log ./results/chain.log
+scp -i ${private_key} ${node_username}@${node_address}:knative/tree.log ./results/tree.log
+scp -i ${private_key} ${node_username}@${node_address}:knative/media-service-test.log ./results/media-service-test.log
+scp -i ${private_key} ${node_username}@${node_address}:knative/hotel-reservation.log ./results/hotel-reservation.log
