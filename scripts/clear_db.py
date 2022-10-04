cc47d5a3aaa01513751c8a81dad61c4c068d3190
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-10-04 16:18:14 -0400

    add fdb import in the call (so that it doesn't get triggered unnecessarily

--- scripts/clear_db.py
+++ scripts/clear_db.py
@@ -1,10 +1,8 @@
 import fdb
 
-fdb.api_version(630)
-db = fdb.open()
-
-
 def main():
+    fdb.api_version(630)
+    db = fdb.open()
     del db[fdb.tuple.range(("data",))]
     del db[fdb.tuple.range(("log",))]
     del db[fdb.tuple.range(("lock",))]
